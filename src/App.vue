dcbaff1df3dc88f9cb911b3b1351a82b111663dd
dhrubajitpc <dhrubajit1992@gmail.com>
2018-09-18 19:35:29 +0800

    initial navbar with css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="navbar">
+      <ul class="navbar__nav-links">
+        <li>Home</li>
+        <li>Favorites</li>
+      </ul>
+      <div class="navbar__search">
+        <input type="text" placeholder="search...">
+        <button>Search</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,12 +27,58 @@ export default {
 </script>
 
 <style>
+:root {
+  --nav-background: #576574;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--nav-background);
+  color: white;
+}
+
+.navbar__nav-links {
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+}
+
+.navbar__nav-links li {
+  padding: 0 10px;
+}
+
+.navbar__search {
+  padding-top: 10px;
+}
+
+.navbar__search input {
+  /* margin-left: 10px; */
+  padding: 10px;
+  background: none;
+  background-color: white;
+  border-radius: 3px;
+  border: solid 1px black;
+}
+
+.navbar__search button {
+  margin: 0 10px;
+  padding: 10px;
+  background: none;
+  border-radius: 3px;
+  font-size: 12px;
+  color: white;
+}
+body {
+  margin: 0;
 }
 </style>
