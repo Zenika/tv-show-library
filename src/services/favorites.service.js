e63d39969007339499854726e60cb056ed416032
Hassen Makni <hassen.makni@zenika.com>
2018-09-27 01:51:37 +0800

    add favorites service

---
+++ src/services/favorites.service.js
@@ -0,0 +1,27 @@
+export default {
+    favorites: [],
+  
+    /**
+       * Add a tvShow to favorites
+       */
+    addFavorite(tvShow) {
+      this.favorites.push(tvShow);
+    },
+  
+    /**
+       * Remove a tvShow from favorites
+       */
+    removeFavorite(tvShow) {
+      const favIndex = this.favorites.findIndex(item => item.id === tvShow.id);
+      this.favorites.splice(favIndex, 1);
+    },
+  
+    /**
+       * Check if the tvShow is part of favorites
+       */
+    isFavorite(tvShow) {
+      return this.favorites.find(item => item.id === tvShow.id);
+    },
+  };
+  
+  
